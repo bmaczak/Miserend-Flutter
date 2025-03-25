55f59aa2b74d2f7f9740743f52ca6ae2ceb39eb2
bmaczak <maczak.balazs@gmail.com>
2025-03-25 15:11:39 +0100

    Increased NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.frama.miserend.hu.miserend"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
